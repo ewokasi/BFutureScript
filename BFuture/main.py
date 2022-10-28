2bf5a1d66151c02cbbf0b38dc51eed2e2f072985
ewokasi <kostyakov0203@mail.ru>
2022-10-29 02:12:10 +0300

    Overrided version
    Fixes and improvments

--- BFuture/main.py
+++ BFuture/main.py
@@ -2,10 +2,8 @@ from binance.spot import Spot
 from binance.lib.utils import config_logging
 from binance.error import ClientError
 import time
-import datetime
+from datetime import datetime
 import logging
-
-
 
 
 def cur_price():
@@ -15,108 +13,127 @@ def cur_price():
 def avg_price():
     return float(client.avg_price("BNBBUSD").get("price"))
 
-client = Spot(key="############################",
-              secret="############################")
 
-if client.get_open_orders("BNBBUSD")!=[]:   #Closing all open orders
-    client.cancel_open_orders("BNBBUSD")
+if __name__ == "__main__":
+    client = Spot(key="####",
+                  secret="###")
 
-for i in client.account().get("balances"):  #Getting data about BNB wallet
-    if i.get("asset") == "BNB":
-        bnb_amount = float(i.get("free"))
+    if client.get_open_orders("BNBBUSD"):  # Closing all open orders
+        client.cancel_open_orders("BNBBUSD")
 
-for i in client.account().get("balances"):  #Getting data about BUSD wallet
-    if i.get("asset") == "BUSD":
-        busd_amount = float(i.get("free"))
-
-if bnb_amount*cur_price()> busd_amount:     #Deciding our to buy or to sell status
-    holded = 1
-else:
-    holded = 0
-up_c = 1.0024 #trade mults
-low_c=0.998
-
-if holded == 1:                             #Setting values for first trades
-    buy_price = cur_price()
-    print("Start price:",buy_price,"Hold status:",holded)
-    print("waiting for ", buy_price * up_c, " of BNB to SELL\n")
-    lastprice = buy_price
-else:
-    sell_price =cur_price()
-    print("Start price:",sell_price,"Hold status:",holded)
-    print("waiting for ", sell_price * low_c, " of BNB to BUY\n")
-    lastprice = sell_price
-
-print("BNB:",bnb_amount,"$: " ,busd_amount)
-
-while 1:
-    for i in client.account().get("balances"): #Every loop updates wallet info
+    for i in client.account().get("balances"):  # Getting data about BNB wallet
         if i.get("asset") == "BNB":
             bnb_amount = float(i.get("free"))
-    for i in client.account().get("balances"):
         if i.get("asset") == "BUSD":
             busd_amount = float(i.get("free"))
 
+    if bnb_amount * cur_price() > busd_amount:  # Deciding to sell or to buy status
+        bnb_hold = 1
+    else:
+        bnb_hold = 0
 
-    current_price = cur_price() # And updates Current price
+    up_c = 1.002  # trade mults
+    low_c = 0.998
+    if bnb_hold == 1:  # Setting values for first trades
+        current = cur_price()
+        average = avg_price()
+
+        if current > average * up_c:
+            up_price = current
+        else:
+            up_price = average * up_c
+
+        print(f"Start at {datetime.now()}\nAvg price: {average}, cur price {current}\n"
+              f"Waiting for {up_price} to SELL\n")
+    else:
+        current = cur_price()
+        average = avg_price()
+
+        if current < average * low_c:
+            low_price = current
+        else:
+            low_price = average * low_c
+
+        print(f"Start at {datetime.now()}\nAvg price: {average}, cur price {current}\n"
+              f"Waiting for {low_price} To Buy\n")
+    lastorder_time = datetime.now()
+    print("BNB:", bnb_amount, "$: ", busd_amount, '\n')
+
+    while 1:
+        current = cur_price()
+
+        for i in client.account().get("balances"):  # Getting data about BNB wallet
+            if i.get("asset") == "BNB":
+                bnb_amount = float(i.get("free"))
+            if i.get("asset") == "BUSD":
+                busd_amount = float(i.get("free"))
 
 
-    try:
-        if holded == 0 and sell_price*low_c>=current_price and client.get_open_orders("BNBBUSD")==[]: #checkes the market
-            params = {
-                'symbol': 'BNBBUSD',
-                'side': 'BUY',
-                'type': 'LIMIT',
-                'timeInForce': 'GTC',
-                'quantity': round((busd_amount/current_price)-0.0002, 3),
-                'price': current_price
-            }
+        if bnb_hold:
+            if current > up_price and client.get_open_orders() == []:
 
-            print("quantity",params.get("quantity"))
-            client.new_order(**params)
-            holded=1
-            buy_price = current_price
-            lastprice = buy_price
-            print("Done buy: ", buy_price, "x", params.get('quantity'),"=",buy_price*params.get('quantity'), datetime.datetime.now())
-            print("BNB ", bnb_amount, "  $ ", busd_amount)
-            print("waiting for ", buy_price*up_c, " of BNB to SELL\n")
+                params = {
+                    'symbol': 'BNBBUSD',
+                    'side': 'SELL',
+                    'type': 'LIMIT',
+                    'timeInForce': 'GTC',
+                    'quantity': round(bnb_amount -0.0006, 3),
+                    'price': current
+                }
+                post = client.new_order(**params)
+                print(f"Order posted{datetime.now()}")
+                low_price = current * low_c
+                bnb_hold = 0
+                lastorder_time = datetime.now()
+                while client.get_open_orders():
+                    time.sleep(10)
+                print(f"\n-------{datetime.now()}-------\nПродажа {params['quantity']} BNB на сумму {params['quantity']*current}\nКурс {current}\nОжидание курса {low_price}\n")
 
-        if holded == 1 and (buy_price*up_c) <=current_price and client.get_open_orders("BNBBUSD")==[]:
-            params = {
-                'symbol': 'BNBBUSD',
-                'side': 'SELL',
-                'type': 'LIMIT',
-                'timeInForce': 'GTC',
-                'quantity': round(bnb_amount-0.0006, 3),
-                'price': current_price
-            }
 
-            print('quantity',round(bnb_amount-0.006, 3))
-            client.new_order(**params)
-            holded=0
-            sell_price = current_price
-            lastprice = sell_price
-            print("Done sell: ",sell_price,"x",params.get('quantity') ,"=", sell_price*params.get('quantity'), datetime.datetime.now())
-            print("BNB ", bnb_amount, "  $ ", busd_amount)
-            print("waiting for ",sell_price*low_c," of BNB to BUY\n")
 
-    except ClientError as error:
-        logging.error(
-            "Found error. status: {}, error code: {}, error message: {}".format(
-                error.status_code, error.error_code, error.error_message
-            )
-        )
-    if lastprice*0.99>current_price:
-        params = {
-            'symbol': 'BNBBUSD',
-            'side': 'SELL',
-            'type': 'LIMIT',
-            'timeInForce': 'GTC',
-            'quantity': round(bnb_amount - 0.0006, 3),
-            'price': current_price
-        }
+        else:
+            if current < low_price and client.get_open_orders() == []:
+                params = {
+                    'symbol': 'BNBBUSD',
+                    'side': 'BUY',
+                    'type': 'LIMIT',
+                    'timeInForce': 'GTC',
+                    'quantity': round(busd_amount / current-0.0006, 3),
+                    'price': current
+                }
 
-        client.new_order(**params)
-        print("Script was stoped automaticly. Price droped to ", current_price)
-        break
-    time.sleep(1)
+                post = client.new_order(**params)
+                bnb_hold = 1
+                up_price = current * up_c
+                lastorder_time =datetime.now()
+                while client.get_open_orders():
+                    time.sleep(10)
+                print(
+                    f"\n-------{datetime.now()}-------\nПокупка {params['quantity']} BNB на сумму {params['quantity'] * current}\nКурс {current}\nОжидание курса {up_price}\n")
+
+        if lastorder_time.hour<=datetime.now().hour-3 and lastorder_time.day==datetime.now().day or datetime.now().hour+19 == lastorder_time.hour and lastorder_time.day==datetime.now().day-1:
+            if client.get_open_orders():
+                client.cancel_open_orders("BNBBUSD")
+
+            if bnb_hold == 1:  # Setting values for first trades
+                current = cur_price()
+                average = avg_price()
+
+                if current > average * up_c:
+                    up_price = current
+                else:
+                    up_price = average * up_c
+
+
+            else:
+                current = cur_price()
+                average = avg_price()
+
+                if current < average * low_c:
+                    low_price = current
+                else:
+                    low_price = average * low_c
+            print(f"Restart at {datetime.now()}\nAvg price: {average}, cur price {current}\n"
+                  f"Waiting for {low_price} to BUY\n")
+            lastorder_time = datetime.now()
+        time.sleep(2)
